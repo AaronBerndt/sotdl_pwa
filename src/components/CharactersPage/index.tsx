f3f799cdac42e881fa2ff6fa3b5a7a9b7b374d37
AaronBerndt <aaron_berndt@DragonSlayer.localdomain>
2021-05-14 10:41:14 -0500

    Centered button

--- src/components/CharactersPage/index.tsx
+++ src/components/CharactersPage/index.tsx
@@ -9,33 +9,35 @@ export default function CharactersPage() {
   const history = useHistory();
 
   return (
-    <Grid container>
+    <Grid container alignContent="center">
       {isLoading === true && characters === undefined ? (
         <p>...Loading</p>
       ) : (
-        <List>
-          {characters.map((character: Character, i: number) => (
-            <ListItem
-              button
-              key={i}
-              onClick={() => {
-                history.push(`/characters/${character.id}`);
-              }}
-            >
-              <ListItemText
-                primary={character.name}
-                secondary={`${character.ancestry} ${
-                  character.novicePath ? character.novicePath : ""
-                } ${character.expertPath ? `.${character.expertPath}` : ""} ${
-                  character.masterPath ? `.${character.masterPath}` : ""
-                } ${character.level}`}
-              />
-            </ListItem>
-          ))}
-        </List>
+        <Grid item xs={12} alignContent="center">
+          <List>
+            {characters.map((character: Character, i: number) => (
+              <ListItem
+                button
+                key={i}
+                onClick={() => {
+                  history.push(`/characters/${character.id}`);
+                }}
+              >
+                <ListItemText
+                  primary={character.name}
+                  secondary={`${character.ancestry} ${
+                    character.novicePath ? character.novicePath : ""
+                  } ${character.expertPath ? `.${character.expertPath}` : ""} ${
+                    character.masterPath ? `.${character.masterPath}` : ""
+                  } ${character.level}`}
+                />
+              </ListItem>
+            ))}
+          </List>
+        </Grid>
       )}
       <Grid item xs={12}>
-        <Button>Create New Character </Button>
+        <Button fullWidth>Create New Character</Button>
       </Grid>
     </Grid>
   );
