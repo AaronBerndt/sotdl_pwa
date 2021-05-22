084b80a2368788fd0c44979a9eba973552be3465
AaronBerndt <aaron.berndt2@gmail.com>
2021-05-21 19:29:16 -0500

    Update api.config.ts

--- src/api.config.ts
+++ src/api.config.ts
@@ -1,4 +1,4 @@
-const URL = "http:fakeapi.com/";
+//const URL = "http:fakeapi.com/";
 
 export const CHARACTER_URL = `/characters`;
 export const UPDATE_CHARACTER_HEALTH_URL = `/update_character_health`;
