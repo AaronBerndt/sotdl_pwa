0481e3b31fcf1edbcf7bcdd9dea341e6c73db540
AaronBerndt <aaron.berndt2@gmail.com>
2021-05-27 14:37:45 -0500

    Feature/character builder page (#6)
    * Added Fate roll logic
    
    * Edit Path Print
    
    * Commented out for now
    
    * Added initial components
    
    * Added PathsList and PathsView
    
    * Added hooks and context
    
    * Added ContentAccordion and AncestryContent
    
    * Tweaked buttons
    
    * Added PathContent
    
    * Added More Level stuff
    
    * Fixed level rendering
    
    * Added more Paths
    
    * Adjusted mocks
    
    * Added Hydrator for prefetching
    
    * Added PathType type
    
    * Added useTalentList
    
    * Added futureLevels
    
    * Restarts Path with goes below level
    
    * Added Change styles
    
    * Fixed level 10 talents
    
    * Fixed CharacteristicList
    
    * Added AttributeAdjuster
    
    * Fixed CharacterBuilderContext for story values
    
    * Fixed build
    
    * Removed path selected view
    
    * Fixed build
    
    Co-authored-by: AaronBerndt <aaron_berndt@DragonSlayer.localdomain>
    Co-authored-by: Aaron <aberndt@nagios.com>

--- src/components/CreateCharacterPage/Organisms/PathSelectedView/PathSelectedView.stories.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Story, Meta } from "@storybook/react/types-6-0";
-import PathSelectedView from "./PathSelectedView";
-export default {
-  title: "CreateCharacterPage/Organisms/PathSelectedView",
-  component: PathSelectedView,
-} as Meta;
-export const Main: Story = () => <PathSelectedView sample="test" />;
