c8cc84abafc4594a283a2d084f9d4141b6791a28
AaronBerndt <aaron_berndt@DragonSlayer.localdomain>
2021-05-30 13:07:53 -0500

    Added AttributeAccordion

---
+++ src/components/CreateCharacterPage/Atoms/AttributeAccordion/AttributeAccordion.tsx
@@ -0,0 +1,91 @@
+import {
+  Badge,
+  Accordion as MuiAccordion,
+  AccordionSummary,
+  Grid,
+  Typography,
+  AccordionDetails,
+  Select,
+  MenuItem,
+} from "@material-ui/core";
+import { ExpandMore } from "@material-ui/icons";
+import { useState } from "react";
+import { Talent } from "../../../CharacterSheetPage/CharacterSheetPageTypes";
+import ErrorIcon from "@material-ui/icons/Error";
+import styled from "styled-components";
+
+export type Props = {
+  talent: Talent;
+};
+
+const Accordion: any = styled(MuiAccordion)`
+  border: 1px solid
+    ${(props: any) =>
+      props?.choicesRemains.length !== props.choiceLimit ? "#1c9aef" : ""};
+`;
+
+export default function AttributeAccordion({ talent }: Props) {
+  const [choices, setChoices] = useState<string[]>([]);
+
+  const onChoiceSelect = (e) => {
+    setChoices(e.target.value);
+  };
+
+  const choiceLimit = talent.description.includes("two") ? 2 : 3;
+
+  return (
+    <Badge
+      invisible={choices.length === choiceLimit}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "left",
+      }}
+      badgeContent={<ErrorIcon style={{ color: "#1c9aef" }} />}
+    >
+      <Accordion choicesRemains={choices} choiceLimit={choiceLimit}>
+        <AccordionSummary expandIcon={<ExpandMore />}>
+          <Grid container>
+            <Grid item xs={12}>
+              <Typography>{talent.name}</Typography>
+            </Grid>
+          </Grid>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Grid container direction="column">
+            <Grid item>
+              <Typography>{talent.description}</Typography>
+            </Grid>
+
+            <Grid item>
+              {choices.length !== choiceLimit && (
+                <Typography>{`Choices Remain: ${
+                  choiceLimit - choices.length
+                }`}</Typography>
+              )}
+            </Grid>
+            <Grid item>
+              <Select
+                multiple
+                value={choices}
+                onChange={onChoiceSelect}
+                renderValue={(selected: any) => selected.join(", ")}
+              >
+                {["Strength", "Agility", "Will", "Intellect"].map((name) => (
+                  <MenuItem
+                    key={name}
+                    value={name}
+                    disabled={
+                      choices.length === choiceLimit && !choices.includes(name)
+                    }
+                  >
+                    {name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </Grid>
+          </Grid>
+        </AccordionDetails>
+      </Accordion>
+    </Badge>
+  );
+}
--- src/components/CreateCharacterPage/Atoms/ChoiceAccordion/ChoiceAccordion.tsx
+++ src/components/CreateCharacterPage/Atoms/ChoiceAccordion/ChoiceAccordion.tsx
@@ -14,69 +14,72 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { Talent } from "../../../CharacterSheetPage/CharacterSheetPageTypes";
 import usePaths from "../../hooks/usePaths";
-import useTalents from "../../hooks/useTalentList";
 import ErrorIcon from "@material-ui/icons/Error";
 export type Props = {
   talent: Talent;
   choicesRemains: boolean;
 };
 
-const Accordion = styled(MuiAccordion)`
-  border: 1px solid lightblue;
+const Accordion: any = styled(MuiAccordion)`
+  border: 1px solid
+    ${(props: any) => (props?.choicesRemains === "" ? "#1c9aef" : "")};
 `;
+
 export default function ChoiceAccordion({ talent, choicesRemains }: Props) {
   const { data: paths } = usePaths();
 
-  const onChoiceSelect = () => {};
+  const [choice, setChoice] = useState("");
+
+  const onChoiceSelect = (e) => {
+    setChoice(e.target.value);
+  };
 
   return (
-    <>
-      <Badge
-        color="primary"
-        variant="dot"
-        anchorOrigin={{
-          vertical: "top",
-          horizontal: "left",
-        }}
-        badgeContent={<ErrorIcon />}
-      >
-        <Accordion>
-          <AccordionSummary expandIcon={<ExpandMore />}>
-            <Grid container>
-              <Grid item xs={10}>
-                <Typography>{talent.name}</Typography>
-              </Grid>
-              <Grid item xs={2}>
-                <Typography>{talent.level}</Typography>
-              </Grid>
+    <Badge
+      invisible={choice !== ""}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "left",
+      }}
+      badgeContent={<ErrorIcon style={{ color: "#1c9aef" }} />}
+    >
+      <Accordion choicesRemains={choice}>
+        <AccordionSummary expandIcon={<ExpandMore />}>
+          <Grid container>
+            <Grid item xs={10}>
+              <Typography>{talent.name}</Typography>
             </Grid>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Grid>
-              <Typography>{talent.description}</Typography>
-              {talent?.choices && (
-                <FormControl>
-                  <Select
-                    native
-                    defaultValue={talent.choices[0]}
-                    onChange={onChoiceSelect}
-                    inputProps={{
-                      name: "age",
-                      id: "age-native-simple",
-                    }}
-                  >
-                    {talent.choices.map((choice: any) => (
-                      <option aria-label={choice.name} value={choice.name}>
-                        {choice.name}
-                      </option>
-                    ))}
-                  </Select>
-                </FormControl>
-              )}
+            <Grid item xs={2}>
+              <Typography>{talent.level}</Typography>
             </Grid>
-          </AccordionDetails>
-        </Accordion>
-      </Badge>
-    </>
+          </Grid>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Grid>
+            <Typography>{talent.description}</Typography>
+            {talent?.choices && (
+              <FormControl>
+                <Select
+                  native
+                  defaultValue={`Pick ${talent} name`}
+                  onChange={onChoiceSelect}
+                  inputProps={{
+                    name: "age",
+                    id: "age-native-simple",
+                  }}
+                >
+                  <option value="" />
+                  {talent.choices.map((choice: any) => (
+                    <option aria-label={choice.name} value={choice.name}>
+                      {choice.name}
+                    </option>
+                  ))}
+                </Select>
+              </FormControl>
+            )}
+          </Grid>
+        </AccordionDetails>
+      </Accordion>
+    </Badge>
   );
 }
