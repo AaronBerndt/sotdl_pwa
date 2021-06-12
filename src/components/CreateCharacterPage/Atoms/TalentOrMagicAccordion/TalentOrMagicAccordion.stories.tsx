1b3cfb7e537f6d226314ca5e118b87f5483a3626
AaronBerndt <aaron_berndt@DragonSlayer.localdomain>
2021-06-12 16:34:37 -0500

    Changed Icon

---
+++ src/components/CreateCharacterPage/Atoms/TalentOrMagicAccordion/TalentOrMagicAccordion.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Story, Meta } from "@storybook/react/types-6-0";
+import TalentOrMagicAccordion from "./TalentOrMagicAccordion";
+import { ancestryList } from "../../CreateCharacterPageMocks";
+export default {
+  title: "CreateCharacterPage/Atoms/TalentOrMagicAccordion",
+  component: TalentOrMagicAccordion,
+} as Meta;
+export const Main: Story = () => (
+  <TalentOrMagicAccordion talent={ancestryList[1].talents[3]} />
+);
