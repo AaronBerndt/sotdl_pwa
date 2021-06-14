e9065321ef171d5243c2c86306af4268fea08add
AaronBerndt <aaron_berndt@DragonSlayer.localdomain>
2021-06-14 16:14:57 -0500

    Added api endpoint

--- src/api.config.ts
+++ src/api.config.ts
@@ -1,15 +1,15 @@
-//const URL = "http:fakeapi.com/";
+const URL = "https://sotl-api-v2.vercel.app/api/";
 
-export const CHARACTER_URL = `/characters`;
-export const UPDATE_CHARACTER_HEALTH_URL = `/update_character_health`;
-export const UPDATE_CHARACTER_AFFLICTIONS = `/update_character_afflictions`;
-export const UPDATE_CHARACTER_EXPENDED_LIST = `/update_character_expended_list`;
-export const UPDATE_GEAR_STATUS_URL = `/update_gear_status`;
-export const UPDATE_CURRENCY_URL = `/update_currency`;
-export const UPDATE_OVERRIDE_URL = `/update_overide`;
+export const CHARACTER_URL = `${URL}/characters`;
+export const UPDATE_CHARACTER_HEALTH_URL = `${URL}/update_character_health`;
+export const UPDATE_CHARACTER_AFFLICTIONS = `${URL}/update_character_afflictions`;
+export const UPDATE_CHARACTER_EXPENDED_LIST = `${URL}/update_character_expended_list`;
+export const UPDATE_GEAR_STATUS_URL = `${URL}/update_gear_status`;
+export const UPDATE_CURRENCY_URL = `${URL}/update_currency`;
+export const UPDATE_OVERRIDE_URL = `${URL}/update_overide`;
 
-export const CREATE_CHARACTER_URL = `/create_character`;
-export const ANCESTRIES_URL = `/ancestries`;
-export const PATH_URL = `/paths`;
-export const SPELLS_URL = `/spells`;
-export const EQUIPMENT_URL = `/equipment`;
+export const CREATE_CHARACTER_URL = `${URL}/create_character`;
+export const ANCESTRIES_URL = `${URL}/ancestries`;
+export const PATH_URL = `${URL}/paths`;
+export const SPELLS_URL = `${URL}/spells`;
+export const EQUIPMENT_URL = `${URL}/equipment`;
