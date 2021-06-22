6d57f1f3f5633d6c0eb589112f56dea30d830fba
AaronBerndt <aaron.berndt2@gmail.com>
2021-06-21 19:06:56 -0500

    Feature/content adder (#14)
    * Edited forms
    
    * Added PathForm
    
    * Fixed onChange of Autocomplete
    
    * Added SpellForm
    
    * Added updates to forms
    
    * Added filters
    
    * Enabled rerender
    
    * Added env var
    
    * worker
    
    Co-authored-by: AaronBerndt <aaron_berndt@DragonSlayer.localdomain>

--- src/index.tsx
+++ src/index.tsx
@@ -10,11 +10,13 @@ import { ReactQueryDevtools } from "react-query/devtools";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/core";
 import theme from "./theme";
-import worker from "./mocks";
 
-if (!process.env.PRODUCTION) {
-  worker.start();
-}
+/* import worker from "./mocks"; */
+
+/* if (!process.env.PRODUCTION) { */
+  /* worker.start(); */
+/* } */
+
 
 const queryClient = new QueryClient();
 
