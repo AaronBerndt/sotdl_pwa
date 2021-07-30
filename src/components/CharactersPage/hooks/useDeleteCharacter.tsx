85e89a39ddeb045652482dd88b53191a849963cf
AaronBerndt <aaron_berndt@DragonSlayer.localdomain>
2021-07-29 19:53:14 -0500

    Fixed delete character

--- src/components/CharactersPage/hooks/useDeleteCharacter.tsx
+++ src/components/CharactersPage/hooks/useDeleteCharacter.tsx
@@ -8,18 +8,21 @@ export default function useDeleteCharacter() {
   const queryClient = useQueryClient();
   return useMutation(
     (characterToDelete: Character) =>
-      axios.delete(`${DELETE_CHARACTER_URL}/_id=${characterToDelete._id}`),
+      axios.delete(`${DELETE_CHARACTER_URL}?_id=${characterToDelete._id}`),
     {
       onMutate: async (characterToDelete: Character) => {
+        console.log(characterToDelete);
         await queryClient.cancelQueries(KEY);
 
         const previousCharacters: any = queryClient.getQueryData(KEY);
 
+        const newCharacterList = previousCharacters.data.filter(
+          ({ _id: characterId }: Character) =>
+            characterId !== characterToDelete._id
+        );
+
         queryClient.setQueryData(KEY, {
-          data: previousCharacters.filter(
-            ({ _id: characterId }: Character) =>
-              characterId === characterToDelete._id
-          ),
+          data: newCharacterList,
         });
 
         return {
