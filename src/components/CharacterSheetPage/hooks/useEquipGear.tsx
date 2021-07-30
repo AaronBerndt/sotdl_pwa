f43cfc2970ed8c62ce2e7734cf44a893ee68e47d
Aaron <aberndt@nagios.com>
2021-07-30 08:31:18 -0500

    Fixed useEquipGear and useUpdateExpendedList

--- src/components/CharacterSheetPage/hooks/useEquipGear.tsx
+++ src/components/CharacterSheetPage/hooks/useEquipGear.tsx
@@ -11,67 +11,70 @@ type MutateProps = {
 
 export default function useEquipGear() {
   const queryClient = useQueryClient();
-  const { id } = useCharacterAttributes();
+  const { _id } = useCharacterAttributes();
 
-  return useMutation((values) => axios.post(UPDATE_GEAR_STATUS_URL, values), {
-    onMutate: async ({ itemToEdit }: MutateProps) => {
-      const CHARACTER_QUERY_KEY = [FETCH_CHARACTER_KEY, id];
+  return useMutation(
+    ({ itemToEdit }) => axios.post(UPDATE_GEAR_STATUS_URL, { _id, itemToEdit }),
+    {
+      onMutate: async ({ itemToEdit }: MutateProps) => {
+        const CHARACTER_QUERY_KEY = [FETCH_CHARACTER_KEY, _id];
 
-      await queryClient.cancelQueries(CHARACTER_QUERY_KEY);
+        await queryClient.cancelQueries(CHARACTER_QUERY_KEY);
 
-      const previousCharacterState: any = queryClient.getQueryData(
-        CHARACTER_QUERY_KEY
-      );
+        const previousCharacterState: any = queryClient.getQueryData(
+          CHARACTER_QUERY_KEY
+        );
 
-      const { items, ...rest } = previousCharacterState.data;
+        const { items, ...rest } = previousCharacterState.data;
 
-      const itemType = itemToEdit.itemType === "armor" ? "armor" : "weapons";
+        const itemType = itemToEdit.itemType === "armor" ? "armor" : "weapons";
 
-      const { [itemType]: itemList, ...itemRest } = items;
-      const newItemArray = itemList.map((oldItem: Weapon | Armor) => {
-        if (oldItem.id === itemToEdit.id) {
-          const { equiped, ...rest } = oldItem;
-          return { ...rest, equiped: !equiped };
-        } else {
-          if (itemType === "armor" && oldItem.equiped) {
+        const { [itemType]: itemList, ...itemRest } = items;
+        const newItemArray = itemList.map((oldItem: Weapon | Armor) => {
+          if (oldItem.id === itemToEdit.id) {
             const { equiped, ...rest } = oldItem;
-            return { ...rest, equiped: false };
+            return { ...rest, equiped: !equiped };
+          } else {
+            if (itemType === "armor" && oldItem.equiped) {
+              const { equiped, ...rest } = oldItem;
+              return { ...rest, equiped: false };
+            }
+            return oldItem;
           }
-          return oldItem;
-        }
-      });
+        });
 
-      const newCharacterState = {
-        items: {
-          [itemType]: newItemArray,
-          ...itemRest,
-        },
-        ...rest,
-      };
+        const newCharacterState = {
+          items: {
+            [itemType]: newItemArray,
+            ...itemRest,
+          },
+          ...rest,
+        };
 
-      queryClient.setQueryData(CHARACTER_QUERY_KEY, {
-        data: newCharacterState,
-      });
+        queryClient.setQueryData(CHARACTER_QUERY_KEY, {
+          data: newCharacterState,
+        });
 
-      return {
-        CHARACTER_QUERY_KEY,
-        previousCharacterState,
-        newCharacterState,
-      };
-    },
-    /* onSuccess: async (data, _, context: any) => { */
-    /*   queryClient.setQueryData( */
-    /*     context?.CHARACTER_QUERY_KEY, */
-    /*     context?.newCharacterState */
-    /*   ); */
-    /* }, */
-    /* onError: async (data, _, context: any) => { */
-    /*   if (context?.previousCharacterState) { */
-    /*     queryClient.setQueryData( */
-    /*       context?.CHARACTER_QUERY_KEY, */
-    /*       context?.newCharacterState */
-    /*     ); */
-    /*   } */
-    /* }, */
-  });
+        return {
+          CHARACTER_QUERY_KEY,
+          previousCharacterState,
+          newCharacterState,
+        };
+      },
+      /* onSuccess: async (data, _, context: any) => { */
+      /*   queryClient.setQueryData( */
+      /*     context?.CHARACTER_QUERY_KEY, */
+      /*     context?.newCharacterState */
+      /*   ); */
+      /* }, */
+      /* onError: async (data, _, context: any) => { */
+      /*   if (context?.previousCharacterState) { */
+      /*     queryClient.setQueryData( */
+      /*       context?.CHARACTER_QUERY_KEY, */
+      /*       context?.newCharacterState */
+      /*     ); */
+      /*   } */
+      /* }, */
+    }
+  );
 }
