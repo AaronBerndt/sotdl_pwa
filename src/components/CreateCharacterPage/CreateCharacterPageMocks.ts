197d77e4dfa237b8162a443358946b1efb09c9df
AaronBerndt <aaron_berndt@DragonSlayer.localdomain>
2021-08-22 20:37:54 -0500

    Changed spell formating

--- src/components/CreateCharacterPage/CreateCharacterPageMocks.ts
+++ src/components/CreateCharacterPage/CreateCharacterPageMocks.ts
@@ -729,14 +729,26 @@ export const pathsList = [
 
 export const spellList = [
   {
+    _id: "60cfaaa49b4a6f765555c2dd",
     name: "Flense",
     tradition: "Air",
     attribute: "Will",
+    damage: "2d6+3",
     type: "Attack",
-    level: 1,
-    damage: "2d6 + 3",
+    level: "1",
     description:
-      "Target One creature or object within short range \n Windborne grit scours your target. Make a Will attack roll against the target’s Strength. On a success, the target takes 2d6 + 3 damage. A living creature that becomes incapacitated by this damage dies instantly, its flesh (if any) stripped from its bones. \n Attack Roll 20+ The target takes 2d6 extra damage.",
+      "Windborne grit scours your target. Make a Will attack roll\nagainst the target’s Strength. On a success, the target\ntakes 2d6 + 3 damage. A living creature that becomes\nincapacitated by this damage dies instantly, its flesh (if any)\nstripped from its bones.",
+    book: "Shadow of the Demon Lord",
+    properties: [
+      {
+        name: "Target",
+        description: "One creature or object within short range",
+      },
+      {
+        name: "Attack Roll 20+",
+        description: "The target takes 2d6 extra damage.",
+      },
+    ],
   },
   {
     name: "Light",
