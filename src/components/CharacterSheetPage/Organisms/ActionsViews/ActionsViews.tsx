cee34b86d908c52d798a7823956cb37bc01e759e
AaronBerndt <aaron_berndt@DragonSlayer.localdomain>
2021-10-12 07:01:41 -0500

    Added Collapse to ActionsView

--- src/components/CharacterSheetPage/Organisms/ActionsViews/ActionsViews.tsx
+++ src/components/CharacterSheetPage/Organisms/ActionsViews/ActionsViews.tsx
@@ -1,10 +1,27 @@
-import { Grid, ListItem, ListItemText } from "@material-ui/core";
+import { Collapse, Grid, ListItem, ListItemText } from "@material-ui/core";
+import { ExpandLess, ExpandMore } from "@material-ui/icons";
 import React from "react";
+import useToggle from "../../../hooks/useToggle";
 import { Talent } from "../../CharacterSheetPageTypes";
 import { useCharacterAttributes } from "../../context/CharacterAttributesContext";
 import WeaponTable from "../../Molecules/WeaponTable/WeaponTable";
 import { actionObject } from "./ActionObject";
-export default function ActionsView() {
+export default function ActionsView(): JSX.Element {
+  const {
+    open: attackActionsOpen,
+    toggleOpen: toggleAttackActionsOpen,
+  } = useToggle();
+
+  const {
+    open: healingOrMoveActionsOpen,
+    toggleOpen: toggleHealingOrMoveActionsOpen,
+  } = useToggle();
+
+  const {
+    open: triggeredActionsOpen,
+    toggleOpen: toggleTriggeredActionsOpen,
+  } = useToggle();
+
   const { talents } = useCharacterAttributes();
   const talentActionList = [
     ...actionObject,
@@ -18,43 +35,61 @@ export default function ActionsView() {
       <Grid item>Weapon</Grid>
       <WeaponTable />
 
-      <Grid item>Attack Action</Grid>
-      {talentActionList
-        .filter(({ description }: any) => description.includes("attack"))
-        .filter(({ description }: any) => !description.includes("triggered"))
-        .filter(({ description }: any) => description.includes("make"))
-        .map((action: any) => (
-          <ListItem>
-            <ListItemText
-              primary={action.name}
-              secondary={action.description}
-            />
-          </ListItem>
-        ))}
+      <Grid item onClick={() => toggleAttackActionsOpen()}>
+        Attack Action
+        {attackActionsOpen ? <ExpandLess /> : <ExpandMore />}
+      </Grid>
 
-      <Grid item>Movement/Healing Action</Grid>
-      {talentActionList
-        .filter(({ description }: any) => !description.includes("make"))
-        .map((action: any) => (
-          <ListItem>
-            <ListItemText
-              primary={action.name}
-              secondary={action.description}
-            />
-          </ListItem>
-        ))}
+      <Collapse in={!attackActionsOpen} timeout="auto" unmountOnExit>
+        {talentActionList
+          .filter(({ description }: any) => description.includes("attack"))
+          .filter(({ description }: any) => !description.includes("triggered"))
+          .filter(({ description }: any) => description.includes("make"))
+          .map((action: any) => (
+            <ListItem>
+              <ListItemText
+                primary={action.name}
+                secondary={action.description}
+              />
+            </ListItem>
+          ))}
+      </Collapse>
 
-      <Grid item>Triggered</Grid>
-      {talentActionList
-        .filter(({ description }: any) => description.includes("triggered"))
-        .map((action: any) => (
-          <ListItem>
-            <ListItemText
-              primary={action.name}
-              secondary={action.description}
-            />
-          </ListItem>
-        ))}
+      <Grid item onClick={() => toggleHealingOrMoveActionsOpen()}>
+        Movement/Healing Action
+        {healingOrMoveActionsOpen ? <ExpandLess /> : <ExpandMore />}
+      </Grid>
+
+      <Collapse in={!healingOrMoveActionsOpen} timeout="auto" unmountOnExit>
+        {talentActionList
+          .filter(({ description }: any) => !description.includes("make"))
+          .map((action: any) => (
+            <ListItem>
+              <ListItemText
+                primary={action.name}
+                secondary={action.description}
+              />
+            </ListItem>
+          ))}
+      </Collapse>
+
+      <Grid item onClick={() => toggleTriggeredActionsOpen()}>
+        Triggered
+        {triggeredActionsOpen ? <ExpandLess /> : <ExpandMore />}
+      </Grid>
+
+      <Collapse in={!triggeredActionsOpen} timeout="auto" unmountOnExit>
+        {talentActionList
+          .filter(({ description }: any) => description.includes("triggered"))
+          .map((action: any) => (
+            <ListItem>
+              <ListItemText
+                primary={action.name}
+                secondary={action.description}
+              />
+            </ListItem>
+          ))}
+      </Collapse>
     </Grid>
   );
 }
