40327e5e81524924171fe215eea949ba05e8c9c0
Aaron <aberndt@nagios.com>
2022-01-11 09:17:03 -0600

    Fixed build

--- src/components/CombatTrackerPage/Atoms/CombatantListItem/CombatantListItem.tsx
+++ src/components/CombatTrackerPage/Atoms/CombatantListItem/CombatantListItem.tsx
@@ -6,7 +6,6 @@ import {
   ListItemText,
 } from "@material-ui/core";
 import React, { useState } from "react";
-import useLongPress from "../../../hooks/useLongPress";
 import useToggle from "../../../hooks/useToggle";
 import { Combatant } from "../../CombatTrackerPageTypes";
 import HurtHealModal from "../../Molecules/HurtHealModal/HurtHealModal";
@@ -30,15 +29,6 @@ export default function CombatantListItem({ combatant }: Props) {
   const [checked, setChecked] = useState(false);
   const toggleEvent = useToggle();
 
-  const longPressEvent = useLongPress(
-    () => {},
-    () => toggleEvent.toggleOpen(),
-    {
-      shouldPreventDefault: true,
-      delay: 500,
-    }
-  );
-
   return (
     <>
       <ListItem button onClick={() => toggleEvent.toggleOpen()}>
@@ -54,7 +44,10 @@ export default function CombatantListItem({ combatant }: Props) {
           }
         />
         <ListItemSecondaryAction>
-          <Checkbox checked={checked} />
+          <Checkbox
+            checked={checked}
+            onClick={() => setChecked((prev) => !prev)}
+          />
         </ListItemSecondaryAction>
       </ListItem>
       <HurtHealModal toggleEvent={toggleEvent} combatant={combatant} />
